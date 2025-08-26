f6f5e6193700d1e652e475b1a46afbede8e5a53e
Your Name <you@example.com>
2025-08-26 09:59:16 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='text-3xl bg-amber-500  text-red-500'>knowledge cafe</h1>
+      
+    </>
+  )
+}
+
+export default App
